fde0bf6e0e35fa2fc2a27290b91b1cfcca4b8278
`Tiago <tiago.pereira.vieira@gmail.com>
2025-08-19 11:12:40 -0300

    GET game by id

--- src/main/java/com/developermobile/dslist3/services/GameService.java
+++ src/main/java/com/developermobile/dslist3/services/GameService.java
@@ -1,9 +1,11 @@
 package com.developermobile.dslist3.services;
 
+import com.developermobile.dslist3.dto.GameDTO;
 import com.developermobile.dslist3.dto.GameMinDTO;
 import com.developermobile.dslist3.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +15,13 @@ public class GameService {
     @Autowired
     private GameRepository repository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return repository.findAll().stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        return new GameDTO(repository.findById(id).get());
     }
 }
